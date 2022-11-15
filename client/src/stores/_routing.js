583e5d7a9708cc547158413de8e10c90b3f11616
Simon Wang <mtwangyugeng@gmail.com>
2022-11-14 19:56:19 -0500

    archetechtures for frontend. next step: apis for core functions

---
+++ client/src/stores/_routing.js
@@ -0,0 +1,43 @@
+function addRoute(storeName, theStore) {
+    theStore.subscribe((v) => {
+        if(v === "") return;
+        // Get the current hash
+        let hash = new URL(window.location.href).hash;
+        
+        // Check if the store name is in the hash
+        const regexR = new RegExp(`&${storeName}=([^&]+)`, 'i');
+        const match = hash.match(regexR);
+
+        // If no match, append the store 
+        if(match === null){ 
+            if(v === "") return;
+            if (hash === "") hash = '#'
+            history.pushState({}, null, hash + `&${storeName}=` + v);
+            return
+        };
+
+        hash.replace(regexR, v)
+        history.pushState({}, null, hash);
+    });
+
+    function traceURL() {
+        console.log("!!!traceURL!!!");
+        const hash = new URL(window.location.href).hash
+        const regexR =  /&(.+)=(.+)/;
+        const match = hash.match(regexR);
+        console.log("!!!traceURL!!!",hash);
+        if(match === null){ 
+            theStore.set("");
+            return
+        };
+
+        const storeNameN = match[1];
+        const targetId = match[2];
+        console.log("!!!traceURL!!!",storeName, targetId);
+        if (storeName === storeNameN) theStore.set(targetId);
+    }
+
+    window.addEventListener('popstate', traceURL)
+    window.addEventListener('hashchange', traceURL)
+    traceURL();
+}
