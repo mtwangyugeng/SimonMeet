9b2750aac564b8e484340961ffa4bb80e1be26d8
Simon Wang <mtwangyugeng@gmail.com>
2022-11-17 12:18:07 -0500

    feature: peers now have glowing border when they are talking

--- client/src/components/inmeetingroom/Peers/Peer.svelte
+++ client/src/components/inmeetingroom/Peers/Peer.svelte
@@ -1,22 +1,52 @@
+<script context=module>
+    import {
+        selectPeerAudioByID,
+        selectDominantSpeaker,
+        selectSpeakers,
+        selectCameraStreamByPeerID
+    } from '@100mslive/hms-video-store';
+
+    const subAudioLevelByPeerId = (peerId, setAudioLevel) => hmsStore.subscribe((level) => setAudioLevel(level), selectPeerAudioByID(peerId));
+
+    console.log('all speakers', hmsStore.getState(selectSpeakers));
+
+    function activeSpeaker(peer, prevPeer) {
+        console.log(`previous active speaker - ${prevPeer.name} with role - ${prevPeer.roleName}`);
+        console.log(`current active speaker - ${peer.name} with role - ${peer.roleName}`);
+    }
+    const subActiveSpeaker = () => hmsStore.subscribe(activeSpeaker, selectDominantSpeaker);
+
+    const subVideoTrackByPeerId = (peerId, setTrack) => hmsStore.subscribe((track) => {
+      setTrack(track);
+    }, selectCameraStreamByPeerID(peerId));
+</script>
+
+
 <script>
     import Video from "./PeerVideo.svelte";
     import {onDestroy} from "svelte";
     import { hmsStore } from "$src/apis/_hms";
-    import {selectCameraStreamByPeerID} from "@100mslive/hms-video-store";
     import InitialAvatar from "$src/components/_common/InitialAvatar.svelte";
 
     export let peer;
 
     let videoTrack;
+    let audioLevel = 0;
 
-    const unsub = hmsStore.subscribe((track) => {
-      videoTrack = track;
-    }, selectCameraStreamByPeerID(peer.id));
+    const unsub = subVideoTrackByPeerId(peer.id, (track) => videoTrack = track)
+    const unsub2 = subAudioLevelByPeerId(peer.id, (level) => audioLevel = level)
+
+    $: outsideGlow = ((level) => {
+        const sigmoid = (num) => 1 / (1 + Math.exp(-num));
+        const color = "lightgreen";
+        return `box-shadow: ${level ? `0px 0px ${24 * sigmoid(level)}px ${color}, 0px 0px ${16 * sigmoid(level)}px ${color}` : ''};`
+    })(audioLevel)
 
     onDestroy(unsub);
+    onDestroy(unsub2);
 </script>
 
-<div class="peer-container">
+<div class="peer-container" style={outsideGlow + "transition: all 0.5s ease-in-out;"}>
     <div class="upper-container">
         {#if (!videoTrack?.enabled || videoTrack?.degraded)}
             <!--show avatar if video is either muted or degraded-->
